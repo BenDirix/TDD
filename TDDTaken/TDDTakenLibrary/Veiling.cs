03aad89d7a0514fc978af39a7c2096ad9d874065
Benjamin Dirix <BenjaminDirix@hotmail.com>
2018-10-29 09:19:57 +0100

    taak 1.2 Veiling

---
+++ TDDTaken/TDDTakenLibrary/Veiling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTakenLibrary
+{
+    public class Veiling
+    {
+        private decimal _hoogsteBod;
+        public void DoeBod(decimal bedrag)
+        {
+            if(bedrag > _hoogsteBod)
+                _hoogsteBod = bedrag;
+        }
+        public decimal HoogsteBod
+        {
+            get
+            {
+                return _hoogsteBod;
+            }
+        }
+    }
+}
---
+++ TDDTaken/TDDTakenLibraryTest/VeilingTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTakenLibrary;
+
+namespace TDDTakenLibraryTest
+{
+    [TestClass]
+    public class VeilingTest
+    {
+        [TestMethod]
+        public void HetHoogsteBodIsNulAlsGeenBodIsUitgevoerd()
+        {
+            var veiling = new Veiling();
+            Assert.AreEqual(0m, veiling.HoogsteBod);
+        }
+        [TestMethod]
+        public void NaEersteBodIsHetHoogsteBodGelijkAanHetBedragVanHetEersteBod()
+        {
+            var bedrag = 30m;
+            var veiling = new Veiling();
+            veiling.DoeBod(bedrag);
+            Assert.AreEqual(bedrag, veiling.HoogsteBod);
+        }
+        [TestMethod]
+        public void NaMeerKeerBiedenIsHetHoogsteBodGelijkAanBedragHoogsteBod()
+        {
+            var veiling = new Veiling();
+            veiling.DoeBod(30m);
+            veiling.DoeBod(50m);
+            veiling.DoeBod(45m);
+            
+            Assert.AreEqual(50m, veiling.HoogsteBod);
+        }
+    }
+}
