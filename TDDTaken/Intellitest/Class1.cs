7ad90fd3a61b38d1235ab61a186bf2710cc4a7f7
Benjamin Dirix <BenjaminDirix@hotmail.com>
2018-10-30 15:11:09 +0100

    Intellitest

---
+++ TDDTaken/Intellitest/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intellitest
+{
+    public class Class1
+    {
+        public bool IsSchrikkelJaar(int jaar)
+        {
+            if(jaar % 400 == 0)
+            {
+                return true;
+            }
+            if(jaar % 100 == 0)
+            {
+                return false;
+            }
+            return jaar % 4 == 0;
+        }
+    }
+}
